e1b1005ce816d2f3f33267428a96260f21048fd0
Abhishek Sahu <rjdeveloper07@gmail.com>
2025-04-02 19:26:03 +0530

    first commit

---
+++ myproject/app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from django.shortcuts import redirect
+
+
+# Create your views here.
+
+def home(request):
+    x="<h1>hello.... </h1>"
+    return HttpResponse(x)
+
+
+
+def about(request):
+    data={'name':True,'age':False, 'quali':None}
+    return JsonResponse(data)
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def otherpage(request):
+    return redirect('https://www.google.com')
+
